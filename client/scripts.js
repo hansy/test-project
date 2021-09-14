27d696e4e47db25230cdb18061a126f0bdb3d126
Hans Yadav <hansy@users.noreply.github.com>
2021-09-14 02:02:19 -0400

    dynamically add reviews

--- client/scripts.js
+++ client/scripts.js
@@ -1,3 +1,82 @@
-$(document).ready(function() {
-  console.log('hello')
+// Converts data from form serialized array into object
+function serializedArrToObj(arr) {
+  return arr.reduce(function (acc, obj) {
+    return { ...acc, [obj.name]: obj.value };
+  }, {});
+}
+
+function getRoundedPercent(n, d) {
+  return Math.round((n / d) * 100);
+}
+
+$(document).ready(function () {
+  function submitForm(url, data, successCallback, errorCallback) {
+    $.ajax(url, {
+      contentType: "application/json",
+      data: JSON.stringify(data),
+      dataType: "json",
+      method: "POST",
+      success: successCallback,
+      error: errorCallback,
+    });
+  }
+  function getFormData(form) {
+    const serializedArray = form.serializeArray();
+    return serializedArrToObj(serializedArray);
+  }
+  function onReviewCreateSuccess(data) {
+    addReview(data);
+    resetCreateReviewForm();
+  }
+  function onReviewCreateError(error) {
+    console.log(error);
+  }
+  function addReview(review) {
+    $(".reviews").prepend(
+      `
+      <li class="review">
+        <div class="rating">
+          <div class="rating__stars">
+            <div class="rating__stars--filled" style="width: ${getRoundedPercent(
+              review.rating,
+              5
+            )}%">
+              <svg><use href="#star"></use></svg>
+              <svg><use href="#star"></use></svg>
+              <svg><use href="#star"></use></svg>
+              <svg><use href="#star"></use></svg>
+              <svg><use href="#star"></use></svg>
+            </div>
+            <div class="rating__stars--empty">
+              <svg><use href="#star"></use></svg>
+              <svg><use href="#star"></use></svg>
+              <svg><use href="#star"></use></svg>
+              <svg><use href="#star"></use></svg>
+              <svg><use href="#star"></use></svg>
+            </div>
+          </div>
+          <span>${review.rating} / 5</span>
+        </div>
+        <p>${review.description}</p>
+      </li>
+      `
+    );
+  }
+
+  function resetCreateReviewForm() {
+    $("#createReviewForm").trigger("reset");
+  }
+
+  $("#createReviewForm").on("submit", function (e) {
+    e.preventDefault();
+
+    const data = getFormData($(this));
+
+    submitForm(
+      "http://localhost:4567/reviews",
+      data,
+      onReviewCreateSuccess,
+      onReviewCreateError
+    );
+  });
 });
--- server/index.rb
+++ server/index.rb
@@ -1,5 +1,28 @@
 require 'sinatra'
+require 'sinatra/cross_origin'
+require 'json'
 
-get '/' do
-  'Howdy'
+configure do
+  enable :cross_origin
+end
+
+before do
+  response.headers['Access-Control-Allow-Origin'] = '*'
+end
+
+options "*" do
+  response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS"
+  response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+  response.headers["Access-Control-Allow-Origin"] = "*"
+  
+  200
+end
+
+post '/reviews' do
+  request.body.rewind
+  data = JSON.parse request.body.read
+
+  content_type :json
+
+  return data.to_json
 end
